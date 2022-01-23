cb45e33cce9ad5a2cad99250d244e691c3207fdd
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-01-24 00:20:57 +0100

    ajout scrollSelect componenet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import ScrollSelect from './components/ScrollSelect/ScrollSelect';
 
 function App() {
+  const services = ['Mars', 'M&m`s', 'Twix', 'Lizard on a stick', 'My foot in your bottom'];
   return (
     <div className="App">
+      <ScrollSelect services={services}/>
     </div>
   );
 }
---
+++ src/components/ScrollSelect/ScrollSelect.js
@@ -0,0 +1,32 @@
+/**
+Component that receive a list of choice from props with the names 'services'
+Display the list as a select element
+handleSubmit as placeholder for further customizations
+ */
+
+import './ScrollSelect.css'
+
+const ScrollSelect = ( { services }) => {
+
+    const handleSelect = (e) => {
+        console.log(e.target.value);
+    }
+
+
+    return (
+        <div className="scrollSelect">
+            <label for="service-select">Choose a service:</label>
+
+            <select name="pets" id="servie-select" onChange={(e) => handleSelect(e)}>
+                <option value="">--Please choose an option--</option>
+                {
+                    services.map((service) => (
+                        <option key={service} value={service}>{ service }</option>
+                    ))
+                }
+            </select>
+        </div>
+    );
+}
+
+export default ScrollSelect;
