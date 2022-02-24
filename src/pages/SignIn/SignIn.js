90c03a41716fd848919f4c6939893dd8a59d0bd8
ImNotAOwl <siegel.matthieu@ymail.com>
2022-02-25 00:48:47 +0100

    add check email + password / it works

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,9 +1,15 @@
 import axios from "axios";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import GenericForm from "../../components/GenericForm/GenericForm";
 
 const SignIn = () => {
-  const signIntext = useRef();
+  
+
+  const signInText = useRef();
+
+  const [emailIsOk, setEmailIsOk] = useState(false);
+  const [pwdIsOk, setPwdIsOk] = useState(false);
+  const [pwdVerifIsOk, setPwdVerifIsOk] = useState(false);
 
   const [toSend, setToSend] = useState([
     { value: "", place: "Nom", id: "nom", type: "text" },
@@ -23,6 +29,7 @@ const SignIn = () => {
 
   const signIn = async (e) => {
     e.preventDefault();
+
     const adress = "http://localhost:3001/users";
     const content = {
       nom: toSend[0].value,
@@ -34,14 +41,62 @@ const SignIn = () => {
       adresse: toSend[3].value,
     };
 
-    const result = await axios.post(adress, content);
-
-    if (result.status === 200) {
-      signIntext.current.innerText = "Vous etes bien inscrit";
+    if ( emailIsOk && pwdIsOk && pwdVerifIsOk ) {
+      const result = await axios.post(adress, content)
+      .catch((error) => {
+        if (error.response.status === 401) {
+            signInText.current.innerText = "Un compte existe déjà avec cette email";
+          }
+      }) 
+     
+      if (result.status === 200) {
+        signInText.current.innerText = "Vous etes bien inscrit";
+      }
     } else {
-      signIntext.current.innerText = "Nous avons recontré un problème";
+        signInText.current.innerText = "Il y a un problème avec votre email ou mot de passe. Veuillez les vérifier";
     }
   };
+
+  useEffect(() => {
+    const pwd = document.getElementById("pwd");
+    const pwdVerif = document.getElementById("verif_pwd");
+    const email = document.getElementById("email");
+
+    // console.log(email, pwd, pwdVerif);
+
+    let patternEmail = new RegExp(
+      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    );
+
+    if (patternEmail.test(email.value)) {
+      email.style.borderBottom = "solid 2px #43aa8b";
+      setEmailIsOk(true);
+    } else {
+      email.style.borderBottom = "solid 2px red";
+      setEmailIsOk(false);
+    }
+
+    const patternPwd = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+    );
+
+    if (patternPwd.test(pwd.value)) {
+      pwd.style.borderBottom = "solid 2px #43aa8b";
+      setPwdIsOk(true);
+    } else {
+      pwd.style.borderBottom = "solid 2px red";
+      setPwdIsOk(false);
+    }
+
+    if (pwd.value === pwdVerif.value && pwdVerif.value !== "" ) {
+        pwdVerif.style.borderBottom = "solid 2px #43aa8b";
+        setPwdVerifIsOk(true);
+    } else {
+        pwdVerif.style.borderBottom = "solid 2px red";
+        setPwdVerifIsOk(false);
+    }
+
+  });
 
   return (
     <div className="signin">
@@ -49,7 +104,7 @@ const SignIn = () => {
         toSend={toSend}
         setToSend={setToSend}
         input={<input type="submit" onClick={signIn} value="Envoyer !" />}
-        par={<p className="signin_ok" ref={signIntext}></p>}
+        par={<p className="signin_ok" ref={signInText}></p>}
       />
     </div>
   );
