3cf8db390b3a8a0c3fef5dbb4f46d918e447ce82
BSevault <b.sevault@gmail.com>
2022-02-25 14:08:55 +0100

    added footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
+import "./Footer.css"
 import pres from "../../assets/docs/Presentation_M2L.pdf"
 
 const Footer = () => {
     return (
-        <div id="footer">
-            <div id="adress">
-                <h2>Maison des Ligues de Lorraine</h2>
+        <ul id="footer">
+            <li id="adress">
+                <h3>Maison des Ligues de Lorraine</h3>
                 <p>12 avenue de Lorraine, 57000 METZ</p>
-            </div>
-            <div className="about-us">
-                <Link to= { pres }>
-                    Qui sommes-nous
-                </Link>
-            </div>
-            <div className="legals">
-                <Link to="/legals">Mentions légales</Link>
-            </div>
-        </div>
+            </li>
+            <li id="presentation">
+                <a href="../../assets/docs/Presentation_M2L.pdf" >
+                   <h3> Présentation des locaux</h3>
+                </a>
+            </li>
+            <li id="legals">
+                <Link to="/legals"><h3>Mentions légales</h3></Link>
+            </li>
+        </ul>
     );
 };
 
