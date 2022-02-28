da646cea272d2ec76dd540df8475445bfb5c5988
ImNotAOwl <siegel.matthieu@ymail.com>
2022-02-28 14:36:38 +0100

    refacto SignIn + customHook

---
+++ src/hooks/useCheckSignIn/useCheckSignIn.js
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+
+const useCheckSignIn = ( email, pwd, pwdVerif ) => {
+
+    useEffect(() => {
+        let patternEmail = new RegExp(
+          /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+        );
+    
+        if (patternEmail.test(email.current.value)) {
+          email.current.style.borderBottom = "solid 2px #43aa8b";
+          email.current.emailIsOk = true;
+        } else {
+          email.current.style.borderBottom = "solid 2px red";
+          email.current.emailIsOk = false;
+        }
+    
+        const patternPwd = new RegExp(
+          "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+        );
+    
+        if (patternPwd.test(pwd.current.value)) {
+          pwd.current.style.borderBottom = "solid 2px #43aa8b";
+          pwd.current.pwdIsOk = true;
+        } else {
+          pwd.current.style.borderBottom = "solid 2px red";
+          pwd.current.pwdIsOk = false;
+        }
+    
+        if (
+          pwd.current.value === pwdVerif.current.value &&
+          pwdVerif.current.value !== ""
+        ) {
+          pwdVerif.current.style.borderBottom = "solid 2px #43aa8b";
+          pwdVerif.current.pwdVerifIsOk = true;
+        } else {
+          pwdVerif.current.style.borderBottom = "solid 2px red";
+          pwdVerif.current.pwdVerifIsOk = false;
+        }
+      });
+
+}
+ 
+export default useCheckSignIn;
