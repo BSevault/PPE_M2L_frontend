c6b11f9fc28e5a8e9677deadb2a1f2629ca9c42f
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-02-28 15:47:43 +0100

    update comment compte

--- src/pages/Compte/Compte.js
+++ src/pages/Compte/Compte.js
@@ -5,37 +5,35 @@ import useAxios from '../../hooks/useAxios/useAxios';
 
 const Compte = ({ user }) => {
 
+  // copy les données utilisateurs moins les données sensibles
   const userFiltered = {...user};
   ['id', 'is_active', 'is_admin', 'ddn'].forEach(e => delete userFiltered[e]);
 
-
+  // définis les données qui vont transiter sur le back
   const [toSend, setToSend] = useState(userFiltered);
   const [content, setContent] = useState();
   const [adress, setAdress] = useState();
   
+  // requête via custom hook, active uniquement à partir du moment ou "adress" est non null
   const { result, error, loading } = useAxios('put', adress, content);
 
+  // Données du formulaire
   const genericFormData = {
     "toSend": toSend,
     "setToSend": setToSend,
   }
 
-  const updateAccount = async () => {
-    // try {
-    //   await axios.put(adress, content)
-    // } catch (error) {
-    //   console.log(error.message);
-    // }
-    
-  }
-
+  // Quand le bouton "envoyez" est cliqué (sur genericForm)
   const handleSubmit = (e) => {
     // prevent page reloading
     e.preventDefault();
+
+    // update l'objet utilisateur avec les données du formulaire
     Object.keys(toSend).forEach((key) => {
       user[key] = toSend[key];
     })
 
+    // setup le body à envoyer
     setContent({
       "nom": user.nom,
       "prenom": user.prenom,
@@ -46,8 +44,8 @@ const Compte = ({ user }) => {
       "adresse": user.adresse
     });
 
+    // setup l'adresse, déclenche le hook axios & la requête
     setAdress(`http://localhost:3001/users/${user.id}`);
-
 
   }
 
