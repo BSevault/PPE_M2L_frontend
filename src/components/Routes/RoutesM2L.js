f225d80e35657e7ce94f59a7f30da1b8383d54a8
BSevault <b.sevault@gmail.com>
2022-03-03 14:37:53 +0100

    fixed /contact route

--- src/components/Routes/RoutesM2L.js
+++ src/components/Routes/RoutesM2L.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Compte from "../../pages/Compte/Compte.js";
+import Contact from "../../pages/Contact/Contact.js";
 import Home from "../../pages/Home/Home.js";
 import Legals from "../../pages/Legals/Legals.js";
 import Login from "../../pages/Login/Login.js";
@@ -19,6 +20,7 @@ const RoutesM2L = ( { user, setUser } ) => {
       <Route path="/compte" element={<Compte user={user} />} />
       <Route path="/legals" element={<Legals />} />
       <Route path="/salles" element={<Salles />} />
+      <Route path="/contact" element={<Contact />} />
 
       {/* <Route path="/backend" element={<ConnectDB />} /> */}
     </Routes>
