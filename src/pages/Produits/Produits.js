7956fcd7a413124ab7c459232d32232bc278d481
BSevault <b.sevault@gmail.com>
2022-03-03 15:38:29 +0100

    💡 modified and fixed Produits page

--- src/pages/Produits/Produits.js
+++ src/pages/Produits/Produits.js
@@ -1,21 +1,22 @@
 import "./Produits.css";
+import useAxios from "../../hooks/useAxios/useAxios";
 import ItemList from "../../components/ItemList/ItemList";
 
 const Produits = () => {
     const productsKeys = ["nom_produit", "description", "qte_dispo", "prix"];
-    const productsHeader = ["Produit", "Description", "Quantité disponible", "Prix"]
+    const productsHeader = ["Produit", "Description", "Quantité disponible", "Prix (€)"]
+    const { response } = useAxios("get", "http://localhost:3001/produits", null );
     return (
         <div id="products-wrapper">
             <div id="products-content">
             <h1>Nos produits disponibles lors de vos évènements</h1>
-                <ItemList
+                { response &&
+                    <ItemList
                     name="products"
-                    method="get"
-                    adress="http://localhost:3001/produits"
-                    content={null}
+                    data={response.success}
                     keys={productsKeys}
                     headers={productsHeader}
-                />
+                /> }
             </div>
         </div>
     );
