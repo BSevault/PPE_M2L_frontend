d1ff69d47d54c0252385fb8dbea6e7b09c84d837
ImNotAOwl <siegel.matthieu@ymail.com>
2022-03-04 00:36:59 +0100

    add disabled day

--- src/pages/Salles/Salles.js
+++ src/pages/Salles/Salles.js
@@ -12,15 +12,22 @@ const Salles = () => {
 
     const {response} = useAxios('get', 'http://localhost:3001/salles/');
 
+    // const {response} = useAxios('get', 'http://localhost:3001/users/all/reservations/');
+
+    console.log(response);
+
     const [ headers, setHeaders ] = useState();
     const [ items, setItems ] = useState();
+    const [ idSalle, setIdSalle ] = useState();
+    
 
     const addData = (e, response) => {
         response.success.forEach( (salle) => {
                 
             if (salle.nom === e.target.textContent || salle.nom === e.target.parentNode.children[1].textContent) {
-                setHeaders([ 'Nom', 'Description', 'Capacité (nb personnes)', 'Prix (€)']);
-                delete salle.id;
+                setHeaders([ 'Nom', 'Description', 'Capacité', 'Prix (€)']);
+                setIdSalle(salle.id);
+                console.log(idSalle);
                 setItems([salle]);
             }
         });
@@ -37,12 +44,25 @@ const Salles = () => {
         addData(e, response);
     }
 
+    const selectDay = (e) => {
+        let jour = new Date(e).toLocaleString().split(",")[0].split("/");
+        jour = `${jour[2]}-${jour[1]}-${jour[0]}`;
+        console.log(jour, idSalle);
+    }
+
     return (  
         <div className="salles">
             <h1>Salles mise à disposition des ligues</h1>
             <PlanSalles selectSalle={selectSalle}/>
-            <ItemList name='salles' keys={keys} headers={headers} data={items}/>
-            <Calendar/>
+            <div className="list_resa">
+                { items &&
+                    <Calendar onClickDay={selectDay} tileDisabled={({activeStartDate, date, view }) => {
+                        console.log(date)
+                        if(date.getDay() === 0 || date.getDay() === 6) return true;
+
+                    }} /> }
+                <ItemList name='salles' keys={keys} headers={headers} data={items}/>
+            </div>
         </div>
     );
 }
