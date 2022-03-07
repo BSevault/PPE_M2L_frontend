3a6a18f0adf4b2ab396c4cb5c04ebab6b42cbae5
ImNotAOwl <siegel.matthieu@ymail.com>
2022-03-07 15:38:22 +0100

    :memo: add comments

--- src/components/SalleResa/SalleResa.js
+++ src/components/SalleResa/SalleResa.js
@@ -10,23 +10,32 @@ const SalleResa = ( {idSalle, dateResevedSalle, user, input, setAllReservations,
     const [ resaConfirm, setResaConfirm] = useState();
     const resa_confirm = useRef();
   
+        // date de débout du calendrier.
     let startDate = new Date();
     
+        // quand on clique sur une date du calendrier
     const selectDay = (e) => {
-        let jour = new Date(e).toLocaleString().split(",")[0].split("/");
-        jour = `${jour[2]}-${jour[1]}-${jour[0]}`;
+            // on formate la date dans le bon sens 
+        let jour = new Date(e).toLocaleDateString().split("/");
+        jour = `${jour[2]}${jour[1]}${jour[0]}`;
+            // on set la date
         setJourSelected(jour);
     }
     
+        // fonction d'envoi de la réservation
     const sendReservation = async () => {
+            // on envoie la réservation au back
         const result = await axios.post(`http://localhost:3001/users/${user.id}/reservation`,
             {date: jourSelected, salle_id: idSalle, is_paid: 0}
         )
+            // set un objet pour l'ajouter au state
         let resa = {date_resa: new Date(jourSelected).toISOString(), is_paid: 0, id_user: user.id, id_salle: idSalle};
 
+            // update du state des reservations par salles
         setDateReservedSalle(prevState => [...prevState, resa]);
         // console.log(dateResevedSalle);
 
+            // si la réservation est confirmé, on affiche un message
         if (result.data.success) {
             setResaConfirm(`La salle est bien réservée pour le ${new Date(jourSelected).toLocaleString().split(",")[0]}`); 
         }
@@ -35,6 +44,7 @@ const SalleResa = ( {idSalle, dateResevedSalle, user, input, setAllReservations,
         }
     }
 
+        // quand on clique quelque part on vide le <p> de confirmation de réservation
     document.onclick = () => {
         setResaConfirm("");
     }
--- src/pages/Salles/Salles.js
+++ src/pages/Salles/Salles.js
@@ -8,9 +8,13 @@ import './Salles.css'
 import 'react-calendar/dist/Calendar.css';
 
 const Salles = ( {user} ) => {
+        // values pour ItemList
     const keys = ['nom', 'description', 'capacite', 'prix'];
 
+        // recupère toutes les salles
     const {response} = useAxios('get', 'http://localhost:3001/salles/');
+
+        // récupère toutes les réservations des salles
     const {response: allResa} = useAxios('get', 'http://localhost:3001/users/all/reservations/');
     
     const [ allReservations, setAllReservations ] = useState();
@@ -19,33 +23,41 @@ const Salles = ( {user} ) => {
     const [ items, setItems ] = useState();
     const [ idSalle, setIdSalle ] = useState(5);
     const [ dateResevedSalle, setDateReservedSalle] = useState();
+    
+    useEffect( () => {
+        setAllReservations(allResa); //set toutes les reservations dans le state allReservations
+    }, [allResa])
 
+        // quand on clique sur une salle du plan.
+    const selectSalle = (e) => {
+            // récupére dans le DOM toutes les balises avec la classe salles_loc
+        const sallesAll = document.querySelectorAll(".salles_loc"); 
+        
+            // on retire toutes les classes salle_active des salles_loc
+        sallesAll.forEach((salle) => {
+            salle.classList.remove("salle_active");
+        });
+        
+            // la salle cliqué on rajoute salle_active
+        e.target.parentNode.classList.add("salle_active");
+        addData(e, response); // appelle la fonction addData
+    }
+    
+        // pour afficher les caracteristiques d'un salle + les résa de la salle selectionné
     const addData = (e, response) => {
+            // on parse toutes les salles
         response.success.forEach( (salle) => {
                 
+                // on vérifie que le nom de la salle correspond au nom où l'on clique
             if (salle.nom === e.target.textContent || salle.nom === e.target.parentNode.children[1].textContent) {
-                setHeaders([ 'Nom', 'Description', 'Capacité', 'Prix (€)']);
-                setIdSalle(salle.id);
-                setItems([salle]);
+                setHeaders([ 'Nom', 'Description', 'Capacité', 'Prix (€)']); // set le header
+                setIdSalle(salle.id); // récup l'id salle
+                setItems([salle]); // récup les données de la salle
+                    // récupère toutes les résa pour la salle selectionnée
                 setDateReservedSalle(allReservations.success.filter( (resa) => resa.id_salle === salle.id ));
             }
         });
     }
-
-    const selectSalle = (e) => {
-        const sallesAll = document.querySelectorAll(".salles_loc");
-
-        sallesAll.forEach((salle) => {
-            salle.classList.remove("salle_active");
-        });
-
-        e.target.parentNode.classList.add("salle_active");
-        addData(e, response); 
-    }
-    
-    useEffect( () => {
-        setAllReservations(allResa);
-    }, [allResa])
 
     return (  
         <div className="salles">
@@ -53,8 +65,9 @@ const Salles = ( {user} ) => {
             <PlanSalles selectSalle={selectSalle}/>
             {items &&
                 <div className="list_resa">
-                    <SalleResa idSalle={idSalle} dateResevedSalle={dateResevedSalle} user={user} input={
-                        <ItemList name='salles' keys={keys} headers={headers} data={items}/>} setAllReservations={setAllReservations} allReservations={allReservations} setDateReservedSalle={setDateReservedSalle} />
+                    <SalleResa idSalle={idSalle} dateResevedSalle={dateResevedSalle} user={user} setAllReservations={setAllReservations} allReservations={allReservations} setDateReservedSalle={setDateReservedSalle} 
+                    input={
+                        <ItemList name='salles' keys={keys} headers={headers} data={items}/>} />
                 </div>
             }
         </div>
