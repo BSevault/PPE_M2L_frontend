50892ebbf8d1a3156e6fc300a450a79c89ca4dcf
BSevault <b.sevault@gmail.com>
2022-03-07 12:17:55 +0100

    on progress

--- src/pages/Complaint/Complaint.js
+++ src/pages/Complaint/Complaint.js
@@ -8,13 +8,13 @@ const Complaint = ( {user} ) => {
 
     const keys = ['id', 'date_ticket', 'description', 'date_probleme', 'nom'];
     const headers = ['Num. ticket', 'Date de création du ticket', 'Description', 'Date du problème', 'Salle concernée' ]
-    const { response } = useAxios('get', `http://localhost:3001/users/${user.id}/tickets`, null);
-    if (response && response.success) {
-        response.success.forEach( e =>{ 
-            console.log(e.date_probleme);
-        })
+    let { response : data } = useAxios('get', `http://localhost:3001/users/${user.id}/tickets`, null);
+    if (data && data.success) {
+        data = data.success; // .toLocaleDateString()
+        console.log(data);
+        }
             
-    }
+
 
         
         
@@ -24,10 +24,10 @@ const Complaint = ( {user} ) => {
             <h1>Réclamations</h1>
             <h2>Faire une réclamation</h2>
             <h2>Vos réclamations</h2>
-            { response &&
+            { data &&
                 <ItemList 
                     name='complaints'
-                    data={response.success}
+                    data={data}
                     keys={keys}
                     headers={headers}
                 />
