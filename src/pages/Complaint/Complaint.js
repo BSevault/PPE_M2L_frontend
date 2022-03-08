192c43441eec14a8241c713c7ec716321d851d12
BSevault <b.sevault@gmail.com>
2022-03-08 15:17:55 +0100

    🐛 fixed date bug

--- src/pages/Complaint/Complaint.js
+++ src/pages/Complaint/Complaint.js
@@ -3,40 +3,71 @@ import "./Complaint.css";
 import ItemList from "../../components/ItemList/ItemList";
 import { useEffect } from "react";
 
-const Complaint = ( {user} ) => {
+const Complaint = ({ user }) => {
+    const keys = ["id", "date_ticket", "description", "date_probleme", "nom"];
+    const headers = [
+        "Num. ticket",
+        "Date de création du ticket",
+        "Description",
+        "Date du problème",
+        "Salle concernée",
+    ];
+    let { response } = useAxios(
+        "get",
+        `http://localhost:3001/users/${user.id}/tickets`,
+        null
+    );
 
-    const keys = ['id', 'date_ticket', 'description', 'date_probleme', 'nom'];
-    const headers = ['Num. ticket', 'Date de création du ticket', 'Description', 'Date du problème', 'Salle concernée' ]
-    let { response } = useAxios('get', `http://localhost:3001/users/${user.id}/tickets`, null);
-    
     useEffect(() => {
         if (response) {
-            console.log(response);
-            response.success.forEach(ticket => {
-                ticket['date_ticket'] = new Date(ticket['date_ticket']).toLocaleDateString();
-                ticket['date_probleme'] = new Date(ticket['date_probleme']).toLocaleDateString();
-                console.log(ticket);
+            // console.log(response);
+            response.success.forEach((ticket) => {
+                ticket["date_ticket"] = new Date(
+                    ticket["date_ticket"]
+                ).toLocaleDateString();
+                ticket["date_probleme"] = new Date(
+                    ticket["date_probleme"]
+                ).toLocaleDateString();
+                // console.log(ticket);
             });
         }
-    },[response])
+    }, [response]);
 
-    return(
-        <div id="complaint">
-            <h1>Réclamations</h1>
-            <h2>Faire une réclamation</h2>
-            <h2>Vos réclamations</h2>
-            { response &&
-                <ItemList 
-                    name='complaints'
-                    data={response.success}
-                    keys={keys}
-                    headers={headers}
-                />
-                
-            }
-        </div>
-    )
-}
+    if (response?.success?.length < 1) {
+        return (
+            <div id="complaint">
+                <h1>Réclamations</h1>
+                <h2>Faire une réclamation</h2>
+                <h2>Vos réclamations</h2>
+                {response && (
+                    <ItemList
+                        name="complaints"
+                        data={response.success}
+                        keys={keys}
+                        headers={headers}
+                    />
+                )}
+                <p>Vous n'avez pas encore soumis de ticket</p>
+            </div>
+        );
+    } else {
+        return (
+            <div id="complaint">
+                <h1>Réclamations</h1>
+                <h2>Faire une réclamation</h2>
+                <h2>Vos réclamations</h2>
+                {response && (
+                    <ItemList
+                        name="complaints"
+                        data={response.success}
+                        keys={keys}
+                        headers={headers}
+                    />
+                )}
+            </div>
+        );
+    }
 
+};
 
 export default Complaint;
