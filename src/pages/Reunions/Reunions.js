e1ecdbfac6d766412675c4abac52df33c63e62ec
Lem0nRavioli <hirona_mj@hotmail.fr>
2022-03-18 19:03:16 +0100

    fix invalid date bug

--- src/pages/Reunions/Reunions.js
+++ src/pages/Reunions/Reunions.js
@@ -10,32 +10,51 @@ const Reunions = () => {
     const { user } = useAuth();
 
     const [displayHistory, setDisplayHistory] = useState(false);
+    const [isSorted, setIsSorted] = useState(false);
 
     const resaKeys = ["nom_salle", "email", "date_resa"];
     const resaHeader = ["Nom de la Salle", "Email de l'organisateur", "Date"];
-    // we can pass a button in resaHeader with handleclick that sort date, cf useEffect for sort function usage (inverse a & b to invert sorting)
-    // const resaHeader = ["Nom de la Salle", "Email de l'organisateur", <ButtonBasic handleClick={() => console.log('clicked in itemlist header')} buttonInnerText="Date"/>];
+    // const resaHeader = ["Nom de la Salle", "Email de l'organisateur", <p onClick={() => sortResponses(isSorted)}>Date</p>];
 
     // 2 request, first for future reunions then history of reunions -- next time setup that with a single request from backend
     const { response } = useAxios("get", `http://localhost:3001/users/${user.id}/participations`, null)
     const { response: responseHistory } = useAxios("get", `http://localhost:3001/users/${user.id}/participations/history`, null)
+
+    
+    /* Marche pas à cause du formatage des dates à la con */
+    // const sortResponses = (isSorted) => {
+    //     if (isSorted) {
+    //         setIsSorted(false);
+    //         if (response) response.success[0].sort((a, b) => new Date(b.date_resa) - new Date(a.date_resa));
+    //         if (responseHistory) responseHistory.success[0].sort((a, b) => new Date(b.date_resa) - new Date(a.date_resa));
+    //     } else {
+    //         setIsSorted(true);
+    //         if (response) response.success[0].sort((a, b) => new Date(a.date_resa) - new Date(b.date_resa));
+    //         if (responseHistory) responseHistory.success[0].sort((a, b) => new Date(a.date_resa) - new Date(b.date_resa));
+    //     }
+    // }
+
 
     useEffect(() => {
         if (response) {
             response.success[0].sort((a, b) => new Date(a.date_resa) - new Date(b.date_resa));
             response.success[0].forEach((resa) => {
                 resa['date_resa'] = new Date(resa['date_resa']).toLocaleDateString('en-GB');
+                // resa['date_resa'] = reformatDate(resa['date_resa']);
             });
         }
+    }, [response])
+
+    useEffect(() => {
 
         if (responseHistory) {
             responseHistory.success[0].sort((a, b) => new Date(b.date_resa) - new Date(a.date_resa));
             responseHistory.success[0].forEach((resa) => {
                 resa['date_resa'] = new Date(resa['date_resa']).toLocaleDateString('en-GB');
+                // resa['date_resa'] = reformatDate(resa['date_resa']);
             });
         }
-
-    }, [response, responseHistory])
+    }, [responseHistory])
 
     if (displayHistory) {
         return (
